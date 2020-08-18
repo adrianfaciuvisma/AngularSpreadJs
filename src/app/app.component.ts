5c92340094af8c55f684422ca3d63566d977e900
Adrian Fâciu <adrian.faciu@gmail.com>
2020-08-18 13:14:36 +0300

    Reproduce spreadjs error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { IO } from '@grapecity/spread-excelio';
+
+import { sampleFile } from '../sample';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularSpreadJs';
+
+  saveComments() {
+    let sampleJson = JSON.stringify(sampleFile);
+
+    const excelIO = new IO();
+    excelIO.save(
+      sampleJson,
+      (res) => console.log('Success: ', res),
+      (err) => console.log('Fail: ', err)
+    );
+  }
+
+  ngOnInit() {
+    this.saveComments();
+  }
 }
